32a575e2d1afe4acb00d9f0daf5b6535ebb954cb
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2023-07-04 23:55:46 +0400

    pkg: update custom and trademark details.

--- data/original/custom.js
+++ data/original/custom.js
@@ -1,32 +1,32 @@
 'use strict';
 
 exports.CUSTOM = [
-  'bit',   // Namecoin
-  'eth',   // ENS
-  'exit',  // Tor
-  'gnu',   // GNUnet (GNS)
-  'i2p',   // Invisible Internet Project
-  'onion', // Tor
-  'tor',   // Tor (OnioNS)
-  'zkey',  // GNS
+  ['bit', 'bit.namecoin.org'], // Namecoin
+  ['eth', 'eth.ens.domains'], // ENS
+  ['exit', 'exit.torproject.com'], // Tor
+  ['gnu', 'gnu.gplv3.com'], // GNUnet (GNS)
+  ['i2p', 'i2p.geti2p.net'], // Invisible Internet Project
+  ['onion', 'onion.torproject.com'], // Tor
+  ['tor', 'tor.torproject.com'], // Tor (OnioNS)
+  ['zkey', 'zkey.gplv3.com'], // GNS
 
   // Required to make the custom values work:
-  'afilias',     // Afilias plc (does not rank)
-  'blockstack',  // Blockstack (does not rank)
-  'brave',       // Brave (english word)
-  'darksi',      // Individual (does not rank)
-  'datprotocol', // Dat Project (does not rank)
-  'debian',      // Debian (prefer over .org)
-  'dnscrypt',    // DNScrypt (does not rank)
-  'eff',         // Electronic Frontier Foundation (prefer over .org)
-  'gnunet',      // GNUnet (does not rank)
-  'keybase',     // Keybase (prefer over keybase.pub)
-  'm-d',         // Individual (does not rank)
-  'marples',     // Individual (does not rank)
-  'mozilla',     // Mozilla Foundation (prefer over .org)
-  'nlnetlabs',   // Unbound (does not rank)
-  'numcalc',     // Individual (does not rank)
-  'pir'          // Public Internet Registry (does not rank)
-].sort((a, b) => {
+  ['afilias', 'afilias.info'], // Afilias plc (does not rank)
+  ['blockstack', 'blockstack.com'], // Blockstack (does not rank)
+  ['brave', 'brave.com'], // Brave (english word)
+  ['darksi', 'darksi.de'], // Individual (does not rank)
+  ['datprotocol', 'datprotocol.com'], // Dat Project (does not rank)
+  ['debian', 'debian.net'], // Debian (prefer over .org)
+  ['dnscrypt', 'dnscrypt.info'], // DNScrypt (does not rank)
+  ['eff', 'eff.com'], // Electronic Frontier Foundation (prefer over .org)
+  ['gnunet', 'gnunet.org'], // GNUnet (does not rank)
+  ['keybase', 'keybase.io'], // Keybase (prefer over keybase.pub)
+  ['m-d', 'm-d.net'], // Individual (does not rank)
+  ['marples', 'marples.name'], // Individual (does not rank)
+  ['mozilla', 'mozilla.com'], // Mozilla Foundation (prefer over .org)
+  ['nlnetlabs', 'nlnetlabs.nl'], // Unbound (does not rank)
+  ['numcalc', 'numcalc.com'], // Individual (does not rank)
+  ['pir', 'pir.org'] // Public Internet Registry (does not rank)
+].sort(([a], [b]) => {
   return a.localeCompare(b);
 });
